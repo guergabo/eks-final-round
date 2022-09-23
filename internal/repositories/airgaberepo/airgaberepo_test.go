d1cc07203932823d1a2912c3eea1f79a66ab5d95
Gabriel Guerra <guergabo@Gabriels-MacBook-Pro.local>
2022-09-23 17:35:08 -0400

    cleaned up repo tests

--- internal/repositories/airgaberepo/airgaberepo_test.go
+++ internal/repositories/airgaberepo/airgaberepo_test.go
@@ -7,11 +7,8 @@ import (
 	"github.com/guergabo/eks-final-round/internal/core/domain"
 )
 
-func TestBookSeatsScenarioSimple(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// "If a customer wants to reserve multiple seats together in the same row, we should be able to accommodate that or tell the customer it's not possible" //
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+func TestBookEntire(t *testing.T) {
+	t.Run("Accomodate entire request or nothing", func(t *testing.T) {
 		mockReq := &domain.Booking{
 			Row:                   domain.A,
 			StartingRowIndex:      0,
@@ -35,23 +32,24 @@ func TestBookSeatsScenarioSimple(t *testing.T) {
 				t.Fatalf("error seat to be book but instead got: %s", seats[i].Status)
 			}
 		}
+	})
+}
 
-		///////////////////////////////////////////////////////////////
-		// "A given seat cannot be reserved by more than one person" //
-		///////////////////////////////////////////////////////////////
-		mockReq = &domain.Booking{
+func TestOnlyOneReservation(t *testing.T) {
+	t.Run("1 seat, 1 reservation allowed", func(t *testing.T) {
+		mockReq := &domain.Booking{
 			Row:                   domain.A,
 			StartingRowIndex:      2,
 			NumOfConsecutiveSeats: 3,
 		}
 
-		mockLf = NewLocalFile()
+		mockLf := NewLocalFile()
 		if err := mockLf.Book(mockReq); err == nil {
 			t.Fatal("expected to handle error")
 		}
 
 		// check if state file was modified
-		airplane, err = loadState()
+		airplane, err := loadState()
 		if err != nil {
 			t.Fatalf("unexpected error: %s", err)
 		}
@@ -66,28 +64,29 @@ func TestBookSeatsScenarioSimple(t *testing.T) {
 				t.Fatalf("error expected seat to be available but instead got: %s", availableSeats[i].Status)
 			}
 		}
+	})
+}
 
-		//////////////////////////////////////////////////////////////////////////////////////////
-		// "If a customer cancels their reservation, the seat is available for reserving again" //
-		//////////////////////////////////////////////////////////////////////////////////////////
+func TestCancellation(t *testing.T) {
+	t.Run("Cancellation makes seat available again", func(t *testing.T) {
 		mockCancelReq := &domain.Cancellation{
 			Row:                   domain.A,
 			StartingRowIndex:      1,
 			NumOfConsecutiveSeats: 2,
 		}
 
-		mockLf = NewLocalFile()
+		mockLf := NewLocalFile()
 		if err := mockLf.Cancel(mockCancelReq); err != nil {
 			t.Fatalf("unexpected error %s", err)
 		}
 
 		// check if state file was modified
-		airplane, err = loadState()
+		airplane, err := loadState()
 		if err != nil {
 			t.Fatalf("unexpected error: %s", err)
 		}
 
-		seats = airplane.Rows[domain.RowID("A")].Seats
+		seats := airplane.Rows[domain.RowID("A")].Seats
 		if seats[0].Status != domain.Booked {
 			t.Fatalf("error expected seat to be booked but instead got: %s", seats[0].Status)
 		}
@@ -100,7 +99,4 @@ func TestBookSeatsScenarioSimple(t *testing.T) {
 	})
 
 	os.Remove("current-state.json")
-}
-
-func TestBookSeatsScenarioExample(t *testing.T) {
 }
