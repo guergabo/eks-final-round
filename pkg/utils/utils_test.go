a7911a346dc4f4c395b70b9fe02a3650ecd21c49
Gabriel Guerra <guergabo@Gabriels-MacBook-Pro.local>
2022-09-23 18:15:39 -0400

    finished utils unit tests:

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -3,10 +3,18 @@ package utils
 import "testing"
 
 func TestUtils(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("Write and Read JSON", func(t *testing.T) {
+		if err := WriteJSONFile("test.json", "mockData"); err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
 
-	})
-	t.Run("", func(t *testing.T) {
+		bs, err := ReadJSONFile("test.json")
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
 
+		if string(bs) == "mockData" {
+			t.Fatalf("expected value 'mockData' instaad got '%s'", string(bs))
+		}
 	})
 }
